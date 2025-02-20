1f633010254b410121f93fe11938e118467342d6
moonprincelol <patrickwang2003@gmail.com>
2025-02-20 18:15:52 +0100

    Added a navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,63 @@
+
+import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+import { Button } from './Button'
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true)
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false)
+
+const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false)
+    } else {
+      setButton(true)
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }
+  , []);
+
+  window.addEventListener('resize', showButton);
+  return (
+    <>
+    <nav className="navbar">
+
+        <div className="navbar-container">
+        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+        GraphAtlas <i class="fa-solid fa-hexagon-nodes"></i>
+        </Link>
+       <div className="menu-icon">
+            <i className={click ? 'fa-solid fa-times' : 'fa-solid fa-bars'}/>
+      </div>
+      <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+      <li className="nav-item">
+      <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+      Home
+      </Link>
+      </li>
+      <li className="nav-item">
+      <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+      Explore
+      </Link>
+      </li>
+      <li className="nav-item">
+      <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+      Submit your own
+      </Link>
+      </li>
+      </ul>
+      {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
